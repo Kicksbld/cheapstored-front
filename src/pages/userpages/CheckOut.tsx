1103d08f9e8b798ae93b33c47587600666f76d95
Killian <killian.boularand@outlook.fr>
2024-12-24 14:51:37 +0100

    checkout started, cart page done

--- src/pages/userpages/CheckOut.tsx
+++ src/pages/userpages/CheckOut.tsx
@@ -5,47 +5,13 @@ import { Typographie } from "@/UI/Design-System/Typographie";
 import Image from "next/image";
 import React from "react";
 
-const Cart = () => {
+const CheckOut = () => {
   return (
     <div className="container">
       <NavBar />
       <div className=" mt-[100px] cart-container mx-auto flex justify-between gap-[100px]">
         <div className="space-y-[35px] flex-[2]">
-          <Typographie font="cooper" variant="h2">
-            Détail de votre Panier
-          </Typographie>
-          <div className="space-y-[15px]">
-            <div className="flex justify-center items-center py-[8px] w-full border rounded-[10px] border-cloud bg-light">
-              <div className="flex items-center gap-[10px] w-fit">
-                <Image
-                  src="/img/svg/bddIcon.svg"
-                  alt=""
-                  width={16}
-                  height={16}
-                />
-                <Typographie
-                  variant="body-sm"
-                  theme="grey"
-                  font="ambit"
-                  weight="regular"
-                >
-                  {" "}
-                  Connecte toi pour une meilleur expérience
-                </Typographie>
-                <Typographie
-                  variant="body-sm"
-                  className="underline cursor-pointer"
-                  theme="grey"
-                  font="ambit"
-                  weight="semibold"
-                >
-                  me connecter
-                </Typographie>
-              </div>
-            </div>
-            <CartProducts />
-            <CartProducts />
-          </div>
+            
         </div>
         <div className="space-y-[35px] flex-1">
           <Typographie font="cooper" variant="h2">
@@ -64,7 +30,7 @@ const Cart = () => {
                 </div>
               </div>
               <Button size="large" className="w-full" variant="outline">
-                Ajouter au panier - 579 €
+                Ajouter un nouveaux code
               </Button>
             </div>
             <div className="p-[20px] border border-cloud bg-light rounded-[10px]">
@@ -94,6 +60,50 @@ const Cart = () => {
                   </Typographie>
                 </div>
                 <hr className="border border-cloud w-full" />
+
+                <div className="flex items-center justify-between">
+                  <div className="flex gap-[20px]">
+                    <div className="w-max h-max">
+                      <Image
+                        src="/img/png/airpod.png"
+                        alt=""
+                        width={54}
+                        height={50}
+                      />
+                    </div>
+
+                    <div className="space-y-[10px]">
+                      <Typographie variant="body-sm" theme="dark" font="ambit">
+                        Apple Airpods Max
+                      </Typographie>
+                      <Typographie
+                        variant="body-sm"
+                        theme="grey"
+                        font="ambit"
+                        className="underline"
+                      >
+                        Retour gratuit sous 3 jours
+                      </Typographie>
+                    </div>
+                  </div>
+                  <div>
+                    <Typographie variant="body-sm" theme="grey" font="ambit">
+                      1 * 434,00€
+                    </Typographie>
+                    <Typographie
+                      className="line-through"
+                      variant="body-sm"
+                      theme="grey"
+                      font="ambit"
+                    >
+                      434,00€
+                    </Typographie>
+                    <Typographie variant="body-sm" theme="modify" font="ambit">
+                      277.00€
+                    </Typographie>
+                  </div>
+                </div>
+                <hr className="border border-cloud w-full" />
                 <div className="flex justify-between items-center">
                   <Typographie font="ambit" variant="body-sm">
                     Total TTC
@@ -103,7 +113,9 @@ const Cart = () => {
                   </Typographie>
                 </div>
                 <hr className="border border-cloud w-full" />
-                <Button size="large" className="w-full" variant="filled">Ajouter Au Panier - 579 €</Button>
+                <Button size="large" className="w-full" variant="filled">
+                  Modifié votre panier
+                </Button>
               </div>
             </div>
           </div>
@@ -113,4 +125,4 @@ const Cart = () => {
   );
 };
 
-export default Cart;
+export default CheckOut;
