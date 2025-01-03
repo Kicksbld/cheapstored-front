d84094eb544ee19d12113d85841290cad745b09d
Killian <killian.boularand@outlook.fr>
2025-01-03 13:52:45 +0100

    login and signin design done

---
+++ src/pages/userpages/LogIn.tsx
@@ -0,0 +1,57 @@
+import { Button } from "@/UI/Design-System/Button";
+import { Input } from "@/UI/Design-System/Input";
+import { Typographie } from "@/UI/Design-System/Typographie";
+import React from "react";
+
+const LogIn = () => {
+  return (
+    <div className="w-full flex ">
+      <div className="hidden flex-1 h-screen bg-signin md:flex"></div>
+      <div className="flex flex-1 items-center justify-center min-h-screen p-4">
+        <div className="space-y-[50px] max-w-[600px]">
+          <div className="space-y-[20px]">
+            <Typographie font="cooper" variant="h2">
+              Bienvenue sur Cheapstored, <br /> Creer un compte pour Continuer.
+            </Typographie>
+            <Typographie font="ambit" variant="tag-title">
+              Vous avez deja un compte?
+              <span className="underline text-blue cursor-pointer">
+                {" "}
+                Connecter vous
+              </span>{" "}
+              <br /> Cela prendra moin de une minutes
+            </Typographie>
+          </div>
+          <form
+            action=""
+            className="flex flex-col items-center gap-[20px] w-full"
+          >
+            <div className="space-y-[10px] w-full">
+              <Typographie variant="h3" font="ambit">
+                Email
+              </Typographie>
+              <Input variant="email" placeholder="email@exemple.com" />
+            </div>
+            <div className="space-y-[10px] w-full">
+              <Typographie variant="h3" font="ambit">
+                Mot de passe
+              </Typographie>
+              <Input variant="password" placeholder="votre mot de passe" />
+            </div>
+            <div className="space-y-[10px] w-full">
+              <Typographie variant="h3" font="ambit">
+                Repéter le Mot de passe
+              </Typographie>
+              <Input variant="password" placeholder="votre mot de passe" />
+            </div>
+            <Button size="large" className="w-full" variant="filled">
+              S'inscrire
+            </Button>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default LogIn;
---
+++ src/pages/userpages/SignIn.tsx
@@ -0,0 +1,57 @@
+import NavBar from "@/UI/Components/navigation/NavBar";
+import { Button } from "@/UI/Design-System/Button";
+import { Input } from "@/UI/Design-System/Input";
+import { Typographie } from "@/UI/Design-System/Typographie";
+import React from "react";
+
+const SignIn = () => {
+  return (
+    <div className="w-full flex ">
+      <div className="hidden flex-1 h-screen bg-login md:flex"></div>
+      <div className="flex flex-1 items-center justify-center min-h-screen p-4">
+        <div className="space-y-[50px] max-w-[600px]">
+          <div className="space-y-[20px]">
+            <Typographie font="cooper" variant="h2">
+              Bienvenue sur Cheapstored, <br /> Connecter vous pour Continuer.
+            </Typographie>
+            <Typographie font="ambit" variant="tag-title">
+              Vous n'avez pas de compte?
+              <span className="underline text-blue cursor-pointer"> Creer un compte</span>{" "}
+              <br /> Cela prendra moin de une minutes
+            </Typographie>
+          </div>
+          <form
+            action=""
+            className="flex flex-col items-center gap-[20px] w-full"
+          >
+            <div className="space-y-[10px] w-full">
+              <Typographie variant="h3" font="ambit">
+                Email
+              </Typographie>
+              <Input variant="email" placeholder="email@exemple.com" />
+            </div>
+            <div className="space-y-[10px] w-full">
+              <Typographie variant="h3" font="ambit">
+                Mot de passe
+              </Typographie>
+              <Input variant="password" placeholder="votre mot de passe" />
+            </div>
+            <Typographie
+              theme="secondary"
+              variant="h3"
+              className="underline cursor-pointer"
+              font="ambit"
+            >
+              Mot de passe oublié?
+            </Typographie>
+            <Button size="large" className="w-full" variant="filled">
+              Se Connecter
+            </Button>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default SignIn;
