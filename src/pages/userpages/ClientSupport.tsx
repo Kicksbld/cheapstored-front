93c37510fcc88dd3fddae9bb9b880db7dc0648d4
Killian <killian.boularand@outlook.fr>
2025-03-07 16:30:55 +0100

    :tada: Ajout d'un bouton pour contacter le support client via mail

--- src/pages/userpages/ClientSupport.tsx
+++ src/pages/userpages/ClientSupport.tsx
@@ -1,9 +1,28 @@
-import React from 'react'
+import { Button } from "@/UI/Design-System/Button";
+import { Typographie } from "@/UI/Design-System/Typographie";
+import React from "react";
+import { useRouter } from "next/router";
 
 const ClientSupport = () => {
+  const router = useRouter();
   return (
-    <div>ClientSupport</div>
-  )
-}
+    <div className="container pb-4 max-w-[500px] flex items-center justify-center min-h-screen w-full">
+      <div className="space-y-[20px] max-w-fit">
+        <Typographie font="cooper" variant="h2">
+          Support Client
+        </Typographie>
+        <Typographie font="ambit" theme="secondary" className="max-w-[500px]">
+          Pour toute question, demande ou problème, notre équipe de support est
+          à votre disposition. N’hésitez pas à nous contacter à l’adresse ci
+          dessous. Nous vous répondrons dans les plus brefs délais !
+        </Typographie>
 
-export default ClientSupport
+        <div onClick={() => router.push("mailto:cheapstored71@gmail.com")}>
+          <Button variant="filled">Nous contacter</Button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ClientSupport;
